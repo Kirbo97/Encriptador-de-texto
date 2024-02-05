909772ab3feeac097aa5a8ba3397bb0f43e10787
Kirbo97 <109368214+Kirbo97@users.noreply.github.com>
2024-02-05 10:44:19 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// añado la funcion para poder remplasar texto
 String.prototype.replaceAt = function(index, replacement) {
 	if (index >= this.length) { return this.valueOf(); }
 	return this.substring(0, index) + replacement + this.substring(index + 1);
 }
 
-// funcion de codificar
+// creo funcion de codificar
 function encriptar(){
-  if(frase.value == ""){ alert("Porfavor ingrese una palabra"); } else {    
+  let fe = '';
+  if(frase.value == ""){ 
+    fe = '!sin codificar';
+    alert("Porfavor ingrese una palabra"); 
+  } else {    
     let ini=0;
-    let fe = '';
     fraini=frase.value;
     var cant = frase.value.length;
     let enc= frase.value.split('');
@@ -20,21 +24,21 @@ function encriptar(){
       fe=fe+enc [ini];
       ini = ini + 1;
     }
-    let parrafo = document.querySelector('.codificar');  //ayuda a conectar elementos espesificos del html
-    parrafo.innerHTML= fe;
-	  //alert(fe);
   }
+  
+  let parrafo = document.querySelector('.codificar');  //ayuda a conectar elementos espesificos del html
+  parrafo.innerHTML= fe;
 }
-
 
 // funcion de decodificar
 function desencriptar(){
   let fd= '';
-  if(frase.value == ""){ alert("Porfavor ingrese una palabra codificada"); } else {
+  if(frase.value == ""){ 
+    fd = '!sin codificar';
+    alert("Porfavor ingrese una palabra codificada"); 
+  } else {
     fd=frase.value.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u");
-    let parrafo = document.querySelector('.codificar');  //ayuda a conectar elementos espesificos del html
-    parrafo.innerHTML= fd;
-   // alert(fd);
   }
+  let parrafo = document.querySelector('.codificar');  //ayuda a conectar elementos espesificos del html
+    parrafo.innerHTML= fd;
 }
-
