ef3397657b5f6d9d09bc569622f828e2677c45da
Kirbo97 <109368214+Kirbo97@users.noreply.github.com>
2024-02-09 13:45:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,39 +6,39 @@ String.prototype.replaceAt = function(index, replacement) {
 
 // creo funcion de codificar
 function encriptar(){
-  let fe = '';
-  if(frase.value == ""){ 
+  let fe = ''; //creo la variable de texto vacio para contener la frase modificado
+  if(frase.value == ""){ // valido si el contenido del imput esta vacio o no a traves de la id
     fe = '!sin codificar';
     alert("Porfavor ingrese una palabra"); 
   } else {    
     let ini=0;
-    fraini=frase.value;
-    var cant = frase.value.length;
-    let enc= frase.value.split('');
+    var cant = frase.value.length; // calculo la cantidad de carracteres que tiene
+    let enc= frase.value.split(''); // creo un arreglo a partir de la frase para cambiar las casillas requeridas
+    // compruebo cada casilla para cambiar las vocales
     while (ini < cant) {
-      if(enc [ini] == "a"){ enc [ini] = 'ai'; }
+      if(enc [ini] == "a"){ enc [ini] = 'ai'; } // remplaso la vocal en la casilla correspondiente
       if(enc [ini] == "e"){ enc [ini] = 'enter'; }
       if(enc [ini] == "i"){ enc [ini] = 'imes'; }
       if(enc [ini] == "o"){ enc [ini] = 'ober'; }
       if(enc [ini] == "u"){ enc [ini] = 'ufat'; }
-      fe=fe+enc [ini];
+      fe=fe+enc [ini]; // unifico todo como una nueva frase
       ini = ini + 1;
     }
   }
   
-  let parrafo = document.querySelector('.codificar');  //ayuda a conectar elementos espesificos del html
-  parrafo.innerHTML= fe;
+  let parrafo = document.querySelector('.codificar');  //la variable se conectara al elementos espesificos del html el cual es el elemento que tenga o este dentro de la clase codificar
+  parrafo.innerHTML= fe; //la variable conectada a la clase remplasara su texto visual por el contenido de la variable "fe"
 }
 
 // funcion de decodificar
 function desencriptar(){
-  let fd= '';
-  if(frase.value == ""){ 
+  let fd= ''; //creo la variable de texto vacio para contener la frase modificado
+  if(frase.value == ""){  // valido si el contenido del imput esta vacio o no a traves de la id
     fd = '!sin codificar';
     alert("Porfavor ingrese una palabra codificada"); 
   } else {
-    fd=frase.value.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u");
+    fd=frase.value.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u"); // mi variable tendra la frase y realisara un remplaso total de todas las coincidencia que tenga
   }
-  let parrafo = document.querySelector('.codificar');  //ayuda a conectar elementos espesificos del html
-    parrafo.innerHTML= fd;
+  let parrafo = document.querySelector('.codificar');  //la variable se conectara al elementos espesificos del html el cual es el elemento que tenga o este dentro de la clase codificar
+    parrafo.innerHTML= fd; //la variable conectada a la clase remplasara su texto visual por el contenido de la variable "fd"
 }
