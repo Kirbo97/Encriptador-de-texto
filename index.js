9895ab45823e4ea61e2242956844cce3f73f56f5
Kirbo97 <109368214+Kirbo97@users.noreply.github.com>
2024-02-10 15:30:37 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,41 +4,85 @@ String.prototype.replaceAt = function(index, replacement) {
 	return this.substring(0, index) + replacement + this.substring(index + 1);
 }
 
+let bol=0;
+
+//creo una funcion que limitara los caracteres a usar en mi cuadro de texto
+function remplazar(elemento){
+  let texto = elemento.value
+  texto = texto.split(/[^a-z, ]+/g) //permito los caracteres de la (a - z) y el espacio en blanco 
+  texto = texto.join("")
+  elemento.value = texto
+}
+
 // creo funcion de codificar
 function encriptar(){
-  let fe = ''; //creo la variable de texto vacio para contener la frase modificado
+  let fe = '!sin codificar'; //creo la variable de texto vacio para contener la frase modificado
   if(frase.value == ""){ // valido si el contenido del imput esta vacio o no a traves de la id
-    fe = '!sin codificar';
     alert("Porfavor ingrese una palabra"); 
+    fe = '!sin codificar';
   } else {    
     let ini=0;
     var cant = frase.value.length; // calculo la cantidad de carracteres que tiene
     let enc= frase.value.split(''); // creo un arreglo a partir de la frase para cambiar las casillas requeridas
     // compruebo cada casilla para cambiar las vocales
     while (ini < cant) {
-      if(enc [ini] == "a"){ enc [ini] = 'ai'; } // remplaso la vocal en la casilla correspondiente
-      if(enc [ini] == "e"){ enc [ini] = 'enter'; }
-      if(enc [ini] == "i"){ enc [ini] = 'imes'; }
-      if(enc [ini] == "o"){ enc [ini] = 'ober'; }
-      if(enc [ini] == "u"){ enc [ini] = 'ufat'; }
-      fe=fe+enc [ini]; // unifico todo como una nueva frase
-      ini = ini + 1;
+
+        if(enc [ini] == "a"){ enc [ini] = 'ai'; } // remplaso la vocal en la casilla correspondiente
+        if(enc [ini] == "e"){ enc [ini] = 'enter'; }
+        if(enc [ini] == "i"){ enc [ini] = 'imes'; }
+        if(enc [ini] == "o"){ enc [ini] = 'ober'; }
+        if(enc [ini] == "u"){ enc [ini] = 'ufat'; }
+
+        if(ini == 0){ fe=enc [ini]; } else { fe=fe+enc [ini]; } // unifico todo como una nueva frase
+        ini = ini + 1;
     }
   }
-  
   let parrafo = document.querySelector('.codificar');  //la variable se conectara al elementos espesificos del html el cual es el elemento que tenga o este dentro de la clase codificar
   parrafo.innerHTML= fe; //la variable conectada a la clase remplasara su texto visual por el contenido de la variable "fe"
+  parrafo.value=fe; //guardo el contenido para ser usado mas adelante
+  bol=1;
+
+  if(fe != "!sin codificar"){   //si el programa codifico la frase, el boton se avilita
+    let btncop = document.getElementById('cop');
+    btncop.disabled = false;
+  }
+
+}
+
+// funcion de decodificar
+function copiar(){
+  let parrafo = document.querySelector('.codificar');  //la variable se conectara al elementos espesificos del html el cual es el elemento que tenga o este dentro de la clase codificar
+  if (bol==1) { //si ya codifico avilita el boton de decodificar
+    let btndes = document.getElementById('desen');
+    btndes.disabled = false;
+    let btnenc = document.getElementById('enc');
+    btnenc.disabled = true;
+  } else if (bol==2) {  //si ya decodifico avilita el boton de codificar
+    let btnenc = document.getElementById('enc');
+    btnenc.disabled = false;
+    let btndes = document.getElementById('desen');
+    btndes.disabled = true;
+  }
+    let btncop = document.getElementById('cop');
+    btncop.disabled = true;
+    frase.value= parrafo.value; //el cuadro de texto remplasara su contenido con lo que tenga el label
 }
 
 // funcion de decodificar
 function desencriptar(){
-  let fd= ''; //creo la variable de texto vacio para contener la frase modificado
+  let fd = '!sin codificar'; //creo la variable de texto vacio para contener la frase modificado
   if(frase.value == ""){  // valido si el contenido del imput esta vacio o no a traves de la id
-    fd = '!sin codificar';
     alert("Porfavor ingrese una palabra codificada"); 
-  } else {
+  } 
+
     fd=frase.value.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u"); // mi variable tendra la frase y realisara un remplaso total de todas las coincidencia que tenga
-  }
   let parrafo = document.querySelector('.codificar');  //la variable se conectara al elementos espesificos del html el cual es el elemento que tenga o este dentro de la clase codificar
     parrafo.innerHTML= fd; //la variable conectada a la clase remplasara su texto visual por el contenido de la variable "fd"
+    parrafo.value=fd; //guardo el contenido para ser usado mas adelante
+    bol=2;
+
+    if(fd != "!sin codificar"){   //si el programa codifico la frase, el boton se avilita
+      let btncop = document.getElementById('cop');
+      btncop.disabled = false;
+    }
 }
